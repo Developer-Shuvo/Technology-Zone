716cfea06214210942a0952fc3af07be8414b151
SHUVO <im.computer.engineeer@gamil.com>
2025-08-03 20:22:39 +0600

    2nd Navbar Complete

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,31 +1,35 @@
 import { Link } from "react-router";
 import trolley from "../assets/images/trolley.png";
+import profile from "../assets/images/user.png";
 
 const Navbar = () => {
   return (
     <>
       <header className="bg-[#f6f6f6] shadow">
-        <div className="max-w-7xl mx-auto flex items-center px-4 py-3">
+        <div className="max-w-[1400px] mx-auto flex items-center  py-3">
           {/* Left Section: Title + Buttons */}
           <div className="flex items-center space-x-2">
-            <h1 className="text-lg font-bold mr-10">Technology Zone</h1>{" "}
+            <h1 className="text-2xl font-bold text-blue-700 mr-12">
+              <span className="text-red-500 text-3xl">Technology</span> Zone
+            </h1>
+
             {/* 40px gap (mr-10) */}
             <button
-              className="px-4 py-2 font-semibold rounded-lg bg-white shadow-lg text-orange-600
+              className="px-4 py-2 font-semibold rounded bg-white shadow-lg text-orange-700
                    transition-all duration-700 ease-in-out
                    hover:bg-sky-800 hover:text-white hover:animate-shake3d"
             >
               Offers
             </button>
             <button
-              className="px-4 py-2 font-semibold rounded-lg bg-white shadow-lg text-orange-500
+              className="px-4 py-2 font-semibold rounded bg-white shadow-lg text-orange-700
                    transition-all duration-700 ease-in-out
                    hover:bg-sky-800 hover:text-white hover:animate-shake3d"
             >
               PC Builder
             </button>
             <button
-              className="px-4 py-2 font-semibold rounded-lg bg-white shadow-lg text-orange-500
+              className="px-4 py-2 font-semibold rounded bg-white shadow-lg text-orange-700
                    transition-all duration-700 ease-in-out
                    hover:bg-sky-800 hover:text-white hover:animate-shake3d"
             >
@@ -65,7 +69,7 @@ const Navbar = () => {
               <Link>
                 {" "}
                 <img
-                  className="h-10 w-10 bg-white rounded-full p-2 shadow-lg  hover:animate-shake3d transition-all duration-700 ease-in-out hover:bg-sky-800"
+                  className="h-10 w-10 bg-white rounded-full p-2 shadow-lg  hover:animate-shake3d transition-all duration-700 ease-in-out hover:bg-yellow-300"
                   src={trolley}
                   alt=""
                 />{" "}
@@ -74,12 +78,346 @@ const Navbar = () => {
               <Link>
                 {" "}
                 <img
-                  className="h-10 w-10 bg-white rounded-full p-2 shadow-lg  hover:animate-shake3d transition-all duration-700 ease-in-out hover:bg-sky-800"
-                  src={trolley}
+                  className="h-10 w-10 bg-white rounded-full p-2 shadow-lg  hover:animate-shake3d transition-all duration-700 ease-in-out hover:bg-yellow-300"
+                  src={profile}
                   alt=""
                 />{" "}
               </Link>
             </div>
+          </div>
+        </div>
+
+        {/* ______________________Second Navbar_________________________ */}
+        <div className="bg-gray-800">
+          <div id="navigation" className="mb-40 max-w-[1400px] mx-auto">
+            <ul
+              id="desktop-nav"
+              className="hidden md:flex justify-center space-x-6"
+            >
+              {/* Phone */}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Smart Phone
+                </a>
+
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              {/* Tablet*/}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Tablet
+                </a>
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              {/* Mac */}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Mac
+                </a>
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              {/* Laptop */}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Laptop
+                </a>
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              {/* Desktop */}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Desktop
+                </a>
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              {/* Accessories */}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Accessories
+                </a>
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              {/* Sound Appliance */}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Sound Appliance
+                </a>
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              {/* Smart Gadget */}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Smart Gadget
+                </a>
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              {/* Smart Watch */}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Smart Watch
+                </a>
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              {/* Monitor */}
+              <li className="relative group">
+                <a
+                  href="#"
+                  className="block px-6 py-4 text-sm font-semibold text-white hover:bg-cyan-800"
+                >
+                  Monitor
+                </a>
+                {/* Dropdown */}
+                <ul
+                  className="absolute top-full right-0 w-40 opacity-0 scale-y-0 group-hover:opacity-100 group-hover:scale-y-100
+               transform origin-top transition-all duration-300 ease-out
+               flex flex-col bg-white border rou-b border-gray-300 shadow-lg  z-10"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Smart Phone
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block w-full text-gray-700 font-semibold text-sm px-4 py-2 text-center hover:bg-cyan-100 transition-colors duration-300 hover:text-blue-900"
+                    >
+                      Featured Phone
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
       </header>
